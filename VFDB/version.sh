c640c79148dcfe1dae884e531631b863ae35a0a1
JingJieChen_Charly <jingjie.chencharly@gmail.com>
2023-02-13 16:43:22 +0800

    Add files via upload

--- VFDB/version.sh
+++ VFDB/version.sh
@@ -1,1 +1,29 @@
 # Last update: Fri Feb 10 19:30:01 2023
+
+# bin directory ----
+bin_dir=""
+# dir for output
+cp=""
+
+time python3 $bin_dir/Step5_makeblastdb.py \
+-c makeblastdb \
+-i $cp"/GENOMES" \
+-o $cp"/DBS" \
+-u 16 \
+-t nucl
+
+
+# before start ----
+"-outfmt", str("6 qseqid sseqid score"),
+
+"-outfmt", str("6"),
+
+
+time python3 $bin_dir/Step5_reciprocal_blast.py \
+-c blastn \
+-i $cp"/" \
+-d $cp"/VFDB_setA_nt.fas" \
+-o $cp"/blast_op" \
+-e 1e-5 \
+-u 16 \
+-m off
